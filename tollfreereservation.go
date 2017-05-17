1f7346f2639e4885d2058f32ec8f8136de7d66a6
anandhirv <anandhi.rv@gmail.com>
2017-05-17 10:14:21 +0530

    Update tollfreereservation.go

--- tollfreereservation.go
+++ tollfreereservation.go
@@ -517,8 +517,7 @@ func (t *NumberPortabilityChaincode) Reserve(stub shim.ChaincodeStubInterface, a
 	// Check the Reseve paramater, if true then update world state with new status
 	
 	var status1 string
-	var key
-	
+	var key string
 	key = args[0]+args[1]
 	Acceptance = args[1]
 	if(Acceptance == "true"){
