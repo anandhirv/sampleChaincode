a5b68b04dd6ecccbfc73fefd741df6195827c2d4
anandhirv <anandhi.rv@gmail.com>
2017-05-17 11:19:33 +0530

    Update tollfreereservation.go

--- tollfreereservation.go
+++ tollfreereservation.go
@@ -552,8 +552,8 @@ func (t *NumberPortabilityChaincode) RegulatorQuery1(stub shim.ChaincodeStubInte
     var key, jsonResp string
     var err error
 
-    if len(args) != 1 {
-        return nil, errors.New("Incorrect number of arguments. Expecting 1 arguments")
+    if len(args) != 2 {
+        return nil, errors.New("Incorrect number of arguments. Expecting 2 arguments")
     }
 
     key = args[0]+args[1]
